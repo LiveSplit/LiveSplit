ece24e74afb7aafadf0a4cea0ce07df7005fdf25
Christopher Serr <cryze92@gmail.com>
2015-05-06 21:09:13 +0200

    Shows the dates the American way from now on
    Even though I dislike that way of showing dates.

--- LiveSplit/LiveSplit.View/View/RunEditorDialog.cs
+++ LiveSplit/LiveSplit.View/View/RunEditorDialog.cs
@@ -1287,7 +1287,7 @@ namespace LiveSplit.View
                         var messageText = formatter.Format(parameters.timeBetween) + " between " + (parameters.startingSegment != null ? parameters.startingSegment.Name : "the start of the run") + " and " + parameters.endingSegment.Name + ", which is faster than the Combined Best Segments of " + formatter.Format(parameters.combinedSumOfBest);
                         if (parameters.attempt.Ended.HasValue)
                         {
-                            messageText += " in a run on " + parameters.attempt.Ended.Value.ToShortDateString();
+                            messageText += " in a run on " + parameters.attempt.Ended.Value.ToString("M/d/yyyy");
                         }
 
                         if (!pastResponses.ContainsKey(messageText))
