2411e733d7337103afb337a4c534bcb4b7122b14
wooferzfg1 <spapushin@gmail.com>
2015-06-08 13:46:33 -0500

    Added ChooseComparisonsDialog

---
+++ LiveSplit/LiveSplit.View/View/ChooseComparisonsDialog.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace LiveSplit.View
+{
+    partial class ChooseComparisonsDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChooseComparisonsDialog));
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.comparisonsListBox = new System.Windows.Forms.CheckedListBox();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnOK.Location = new System.Drawing.Point(121, 152);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 16;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(202, 152);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 17;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel3.Controls.Add(this.btnCancel, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.btnOK, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.comparisonsListBox, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(7, 7);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(280, 178);
+            this.tableLayoutPanel3.TabIndex = 41;
+            // 
+            // comparisonsListBox
+            // 
+            this.tableLayoutPanel3.SetColumnSpan(this.comparisonsListBox, 2);
+            this.comparisonsListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comparisonsListBox.FormattingEnabled = true;
+            this.comparisonsListBox.Location = new System.Drawing.Point(3, 3);
+            this.comparisonsListBox.Name = "comparisonsListBox";
+            this.comparisonsListBox.Size = new System.Drawing.Size(274, 143);
+            this.comparisonsListBox.TabIndex = 18;
+            this.comparisonsListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.comparisonsListBox_ItemCheck);
+            // 
+            // ChooseComparisonsDialog
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 192);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(520, 10000);
+            this.MinimumSize = new System.Drawing.Size(310, 230);
+            this.Name = "ChooseComparisonsDialog";
+            this.Padding = new System.Windows.Forms.Padding(7);
+            this.Text = "Choose Comparisons";
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.CheckedListBox comparisonsListBox;
+
+    }
+}
---
+++ LiveSplit/LiveSplit.View/View/ChooseComparisonsDialog.cs
@@ -0,0 +1,46 @@
+﻿using LiveSplit.Model.Comparisons;
+using LiveSplit.UI;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LiveSplit.View
+{
+    public partial class ChooseComparisonsDialog : Form
+    {
+        public IDictionary<string, bool> ComparisonGeneratorStates { get; set; }
+
+        public ChooseComparisonsDialog()
+        {
+            InitializeComponent();
+
+            comparisonsListBox.Items.AddRange(new []
+            {
+                BestSegmentsComparisonGenerator.ComparisonName,
+                BestSplitTimesComparisonGenerator.ComparisonName,
+                AverageSegmentsComparisonGenerator.ComparisonName,
+                PercentileComparisonGenerator.ComparisonName,
+                NoneComparisonGenerator.ComparisonName
+            });
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void comparisonsListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            ComparisonGeneratorStates[(string)comparisonsListBox.Items[e.Index]] = e.NewValue == CheckState.Checked;
+        }
+    }
+}
