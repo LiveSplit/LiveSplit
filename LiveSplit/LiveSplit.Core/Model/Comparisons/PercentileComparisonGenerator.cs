85e2e26b4afd3d22adbd87445719aaa15d9b0454
wooferzfg1 <spapushin@gmail.com>
2015-07-04 17:31:43 -0500

    Fix history not being used properly in Comparison Generators
    Resolves #276

--- LiveSplit/LiveSplit.Core/Model/Comparisons/PercentileComparisonGenerator.cs
+++ LiveSplit/LiveSplit.Core/Model/Comparisons/PercentileComparisonGenerator.cs
@@ -57,7 +57,7 @@ namespace LiveSplit.Model.Comparisons
                         }
                         else ignoreNextHistory = false;
                     }
-                    else break;
+                    else ignoreNextHistory = false;
                 }
             }
 
