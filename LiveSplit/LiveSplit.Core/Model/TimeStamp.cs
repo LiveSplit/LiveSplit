73b73722187deeeb8a46c8939758e24a65c05cde
wooferzfg1 <spapushin@gmail.com>
2015-08-14 17:51:00 -0500

    Use an NTP server instead of NIST
    This allows the use of milliseconds, which prevents rounding errors.

--- LiveSplit/LiveSplit.Core/Model/TimeStamp.cs
+++ LiveSplit/LiveSplit.Core/Model/TimeStamp.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
@@ -77,12 +78,12 @@ namespace LiveSplit.Model
             {
                 try
                 {
-                    var nistTime = GetNISTDate();
+                    var networkTime = GetNetworkTime();
                     var qpcTime = qpc.Elapsed;
                     if (firstQPCTime != TimeSpan.Zero)
                     {
                         var qpcDelta = qpcTime - firstQPCTime;
-                        var nistDelta = nistTime - firstNISTTime;
+                        var nistDelta = networkTime - firstNISTTime;
 
                         var newAccumulatedTime = accumulatedTime;
                         newAccumulatedTime.QPC = persistentAccumulatedTime.QPC + qpcDelta;
@@ -95,7 +96,7 @@ namespace LiveSplit.Model
                     else
                     {
                         firstQPCTime = qpcTime;
-                        firstNISTTime = nistTime;
+                        firstNISTTime = networkTime;
                     }
                 }
                 catch { }
@@ -103,58 +104,68 @@ namespace LiveSplit.Model
             }
         }
 
-        private static DateTime GetNISTDate()
+        public static DateTime GetNetworkTime()
         {
-            Random ran = new Random(DateTime.Now.Millisecond);
-            string serverResponse = string.Empty;
+            var before = Now;
+            TimeStamp after;
 
-            // Represents the list of NIST servers
-            string[] servers = new string[] {
-                /*"nist1-ny.ustiming.org",
-                "time-a.nist.gov",
-                "nist1-chi.ustiming.org",*/
-                "time.nist.gov",
-                /*"ntp-nist.ldsbc.edu",
-                "nist1-la.ustiming.org"       */                  
-            };
+            //default Windows time server
+            const string ntpServer = "time.windows.com";
 
-            // Try each server in random order to avoid blocked requests due to too frequent request
-            for (int i = 0; i < 5; i++)
-            {
-                var before = Now;
-                TimeStamp after;
+            // NTP message size - 16 bytes of the digest (RFC 2030)
+            var ntpData = new byte[48];
 
-                // Open a StreamReader to a random time server
-                using (var tcpClient = new TcpClient(servers[ran.Next(0, servers.Length)], 13))
-                using (var reader = new StreamReader(tcpClient.GetStream()))
-                {
-                    after = Now;
-                    serverResponse = reader.ReadToEnd();
-                }
+            //Setting the Leap Indicator, Version Number and Mode values
+            ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
 
-                var delta = TimeSpan.FromMilliseconds((after - before).TotalMilliseconds / 2);
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
 
-                // Check to see that the signature is there
-                if (serverResponse.Length > 47 && serverResponse.Substring(38, 9).Equals("UTC(NIST)"))
-                {
-                    // Parse the date
-                    int jd = int.Parse(serverResponse.Substring(1, 5));
-                    int yr = int.Parse(serverResponse.Substring(7, 2));
-                    int mo = int.Parse(serverResponse.Substring(10, 2));
-                    int dy = int.Parse(serverResponse.Substring(13, 2));
-                    int hr = int.Parse(serverResponse.Substring(16, 2));
-                    int mm = int.Parse(serverResponse.Substring(19, 2));
-                    int sc = int.Parse(serverResponse.Substring(22, 2));
+            //The UDP port number assigned to NTP is 123
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            //NTP uses UDP
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-                    if (jd > 51544)
-                        yr += 2000;
-                    else
-                        yr += 1999;
+            socket.Connect(ipEndPoint);
 
-                    return new DateTime(yr, mo, dy, hr, mm, sc) + delta;
-                }
-            }
-            throw new Exception();
+            //Stops code hang if NTP is blocked
+            socket.ReceiveTimeout = 3000;
+
+            socket.Send(ntpData);
+            socket.Receive(ntpData);
+            socket.Close();
+
+            //Offset to get to the "Transmit Timestamp" field (time at which the reply 
+            //departed the server for the client, in 64-bit timestamp format."
+            const byte serverReplyTime = 40;
+
+            //Get the seconds part
+            ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
+
+            //Get the seconds fraction
+            ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
+
+            //Convert From big-endian to little-endian
+            intPart = SwapEndianness(intPart);
+            fractPart = SwapEndianness(fractPart);
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+
+            //**UTC** time
+            var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long)milliseconds);
+
+            after = Now;
+            var delta = TimeSpan.FromMilliseconds((after - before).TotalMilliseconds / 2);
+
+            return networkDateTime + delta;
+        }
+
+        // stackoverflow.com/a/3294698/162671
+        static uint SwapEndianness(ulong x)
+        {
+            return (uint)(((x & 0x000000ff) << 24) +
+                           ((x & 0x0000ff00) << 8) +
+                           ((x & 0x00ff0000) >> 8) +
+                           ((x & 0xff000000) >> 24));
         }
 
         public static TimeSpan operator -(TimeStamp a, TimeStamp b)
