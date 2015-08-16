c5913e67d5de8229e519baa5e1fb02e7ed24a39b
Christopher Serr <cryze92@gmail.com>
2015-08-16 15:20:14 +0200

    Refactor the NTP algorithm into its own class
    The NTP algorithm is now publically available through NTP.Now.

--- LiveSplit/LiveSplit.Core/Model/TimeStamp.cs
+++ LiveSplit/LiveSplit.Core/Model/TimeStamp.cs
@@ -60,7 +60,7 @@ namespace LiveSplit.Model
                 {
                     try
                     {
-                        ntpTime = GetNetworkTime();
+                        ntpTime = NTP.Now;
                         qpcTime = qpc.Elapsed;
                         times.Add(ntpTime.Ticks - qpcTime.Ticks);
                     }
@@ -93,72 +93,6 @@ namespace LiveSplit.Model
                 }
                 else Thread.Sleep(TimeSpan.FromHours(0.5));
             }
-        }
-
-        // stackoverflow.com/a/12150289
-        public static DateTime GetNetworkTime()
-        {
-            //default Windows time server
-            const string ntpServer = "time.windows.com";
-
-            // NTP message size - 16 bytes of the digest (RFC 2030)
-            var ntpData = new byte[48];
-
-            //Setting the Leap Indicator, Version Number and Mode values
-            ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
-
-            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
-            
-            //The UDP port number assigned to NTP is 123
-            var ipEndPoint = new IPEndPoint(addresses[0], 123);
-            //NTP uses UDP
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-
-            socket.Connect(ipEndPoint);
-
-            //Stops code hang if NTP is blocked
-            socket.ReceiveTimeout = 3000;
-
-            var before = Now;
-            TimeStamp after;
-
-            socket.Send(ntpData);
-            socket.Receive(ntpData);
-
-            after = Now;
-            var delta = TimeSpan.FromMilliseconds((after - before).TotalMilliseconds / 2);
-
-            socket.Close();
-
-            //Offset to get to the "Transmit Timestamp" field (time at which the reply 
-            //departed the server for the client, in 64-bit timestamp format."
-            const byte serverReplyTime = 40;
-
-            //Get the seconds part
-            ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
-
-            //Get the seconds fraction
-            ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
-
-            //Convert From big-endian to little-endian
-            intPart = SwapEndianness(intPart);
-            fractPart = SwapEndianness(fractPart);
-
-            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
-
-            //**UTC** time
-            var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long)milliseconds);
-
-            return networkDateTime + delta;
-        }
-
-        // stackoverflow.com/a/3294698
-        static uint SwapEndianness(ulong x)
-        {
-            return (uint)(((x & 0x000000ff) << 24) +
-                           ((x & 0x0000ff00) << 8) +
-                           ((x & 0x00ff0000) >> 8) +
-                           ((x & 0xff000000) >> 24));
         }
 
         public static TimeSpan operator -(TimeStamp a, TimeStamp b)
