6f96325de97824a487f9482abc55206bdccaa633
Fatalis <fatalis.twitch@gmail.com>
2015-08-22 20:33:01 -0500

    Add shared game memory utils to Core DeepPointer, SignatureScanner, and process.ReadValue
    ASL and DLL components will use this

--- LiveSplit/LiveSplit.Core/ComponentUtil/ProcessExtensions.cs
+++ LiveSplit/LiveSplit.Core/ComponentUtil/ProcessExtensions.cs
@@ -9,8 +9,7 @@ using System.Text;
 
 namespace LiveSplit.ComponentUtil
 {
-    // both ASL and DLL autosplitters should use this as a replacement for Process.Modules
-    // it's required for 64<->32 when accessing a module other than mainmodule
+    using SizeT = UIntPtr;
 
     public class ProcessModuleWow64Safe
     {
@@ -25,19 +24,38 @@ namespace LiveSplit.ComponentUtil
         }
     }
 
+    public enum ReadStringType
+    {
+        AutoDetect,
+        ASCII,
+        UTF8,
+        UTF16
+    }
+
     public static class ExtensionMethods
     {
-        [DllImport("psapi.dll", SetLastError = true)]
-        public static extern bool EnumProcessModulesEx(IntPtr hProcess, [Out] IntPtr[] lphModule, uint cb, out uint lpcbNeeded, uint dwFilterFlag);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer,
+            SizeT dwSize, out SizeT lpNumberOfBytesRead);
 
         [DllImport("psapi.dll", SetLastError = true)]
-        static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, uint nSize);
+        public static extern bool EnumProcessModulesEx(IntPtr hProcess, [Out] IntPtr[] lphModule, uint cb,
+            out uint lpcbNeeded, uint dwFilterFlag);
 
         [DllImport("psapi.dll", SetLastError = true)]
-        public static extern bool GetModuleInformation(IntPtr hProcess, IntPtr hModule, [Out] out MODULEINFO lpmodinfo, uint cb);
+        private static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName,
+            uint nSize);
+
+        [DllImport("psapi.dll", SetLastError = true)]
+        public static extern bool GetModuleInformation(IntPtr hProcess, IntPtr hModule, [Out] out MODULEINFO lpmodinfo,
+            uint cb);
 
         [DllImport("psapi.dll")]
-        static extern uint GetModuleBaseName(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, uint nSize);
+        private static extern uint GetModuleBaseName(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName,
+            uint nSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool IsWow64Process(IntPtr hProcess, [Out] out bool wow64Process);
 
         [StructLayout(LayoutKind.Sequential)]
         public struct MODULEINFO
@@ -106,6 +124,203 @@ namespace LiveSplit.ComponentUtil
             ModuleCache.Add(hash, ret.ToArray());
 
             return ret.ToArray();
+        }
+
+        public static bool Is64Bit(this Process process)
+        {
+            bool procWow64;
+            IsWow64Process(process.Handle, out procWow64);
+            if (Environment.Is64BitOperatingSystem && !procWow64)
+                return true;
+            return false;
+        }
+
+        public static bool ReadValue<T>(this Process process, IntPtr addr, out T val) where T : struct
+        {
+            var type = typeof(T);
+            type = type.IsEnum ? Enum.GetUnderlyingType(type) : type;
+
+            val = default(T);
+            object val2;
+            if (!ReadValue(process, addr, type, out val2))
+                return false;
+
+            val = (T)val2;
+
+            return true;
+        }
+
+        public static bool ReadValue(Process process, IntPtr addr, Type type, out object val)
+        {
+            byte[] bytes;
+
+            val = null;
+            int size = type == typeof(bool) ? 1 : Marshal.SizeOf(type);
+            if (!ReadBytes(process, addr, size, out bytes))
+                return false;
+
+            val = ResolveToType(bytes, type);
+
+            return true;
+        }
+
+        public static bool ReadBytes(this Process process, IntPtr addr, int count, out byte[] val)
+        {
+            var bytes = new byte[count];
+
+            SizeT read;
+            val = null;
+            if (!ReadProcessMemory(process.Handle, addr, bytes, (SizeT)bytes.Length, out read)
+                || read != (SizeT)bytes.Length)
+                return false;
+
+            val = bytes;
+
+            return true;
+        }
+
+        public static bool ReadPointer(this Process process, IntPtr addr, out IntPtr val)
+        {
+            return ReadPointer(process, addr, process.Is64Bit(), out val);
+        }
+
+        public static bool ReadPointer(this Process process, IntPtr addr, bool is64Bit, out IntPtr val)
+        {
+            var bytes = new byte[is64Bit ? 8 : 4];
+
+            SizeT read;
+            val = IntPtr.Zero;
+            if (!ReadProcessMemory(process.Handle, addr, bytes, (SizeT)bytes.Length, out read)
+                || read != (SizeT)bytes.Length)
+                return false;
+
+            val = is64Bit ? (IntPtr)BitConverter.ToInt64(bytes, 0) : (IntPtr)BitConverter.ToInt32(bytes, 0);
+
+            return true;
+        }
+
+        public static bool ReadString(this Process process, IntPtr addr, int len, out string str)
+        {
+            return ReadString(process, addr, ReadStringType.AutoDetect, len, out str);
+        }
+
+        public static bool ReadString(this Process process, IntPtr addr, ReadStringType type, int len, out string str)
+        {
+            var sb = new StringBuilder(len);
+            if (!ReadString(process, addr, type, sb))
+            {
+                str = String.Empty;
+                return false;
+            }
+
+            str = sb.ToString();
+
+            return true;
+        }
+
+        public static bool ReadString(this Process process, IntPtr addr, StringBuilder sb)
+        {
+            return ReadString(process, addr, ReadStringType.AutoDetect, sb);
+        }
+
+        public static bool ReadString(this Process process, IntPtr addr, ReadStringType type, StringBuilder sb)
+        {
+            var bytes = new byte[sb.Capacity];
+            SizeT read;
+            if (!ReadProcessMemory(process.Handle, addr, bytes, (SizeT)bytes.Length, out read)
+                || read != (SizeT)bytes.Length)
+                return false;
+
+            if (type == ReadStringType.AutoDetect)
+            {
+                if (read.ToUInt64() >= 2 && bytes[1] == '\x0')
+                    sb.Append(Encoding.Unicode.GetString(bytes));
+                else
+                    sb.Append(Encoding.UTF8.GetString(bytes));
+            }
+            else if (type == ReadStringType.UTF8)
+                sb.Append(Encoding.UTF8.GetString(bytes));
+            else if (type == ReadStringType.UTF16)
+                sb.Append(Encoding.Unicode.GetString(bytes));
+            else
+                sb.Append(Encoding.ASCII.GetString(bytes));
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (sb[i] == '\0')
+                {
+                    sb.Remove(i, sb.Length - i);
+                    break;
+                }
+            }
+
+            return true;
+        }
+
+        static object ResolveToType(byte[] bytes, Type type)
+        {
+            object val;
+
+            if (type == typeof(int))
+            {
+                val = BitConverter.ToInt32(bytes, 0);
+            }
+            else if (type == typeof(uint))
+            {
+                val = BitConverter.ToUInt32(bytes, 0);
+            }
+            else if (type == typeof(float))
+            {
+                val = BitConverter.ToSingle(bytes, 0);
+            }
+            else if (type == typeof(double))
+            {
+                val = BitConverter.ToDouble(bytes, 0);
+            }
+            else if (type == typeof(byte))
+            {
+                val = bytes[0];
+            }
+            else if (type == typeof(bool))
+            {
+                if (bytes == null)
+                    val = false;
+                else
+                    val = (bytes[0] != 0);
+            }
+            else if (type == typeof(short))
+            {
+                val = BitConverter.ToInt16(bytes, 0);
+            }
+            else // probably a struct
+            {
+                var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+                try
+                {
+                    val = Marshal.PtrToStructure(handle.AddrOfPinnedObject(), type);
+                }
+                finally
+                {
+                    handle.Free();
+                }
+            }
+
+            return val;
+        }
+
+        public static unsafe float ToFloatBits(this uint i)
+        {
+            return *((float*)&i);
+        }
+
+        public static unsafe uint ToUInt32Bits(this float f)
+        {
+            return *((uint*)&f);
+        }
+
+        public static bool BitEquals(this float f, float o)
+        {
+            return ToUInt32Bits(f) == ToUInt32Bits(o);
         }
     }
 }
---
+++ LiveSplit/LiveSplit.Core/ComponentUtil/SignatureScanner.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Globalization;
+#pragma warning disable 1591
+
+namespace LiveSplit.ComponentUtil
+{
+    public class SignatureScanner
+    {
+        private byte[] _memory;
+        private Process _process;
+        private IntPtr _address;
+        private int _size;
+
+        public IntPtr Address
+        {
+            get { return _address; }
+            set {
+                _memory = null;
+                _address = value;
+            }
+        }
+
+        public int Size
+        {
+            get { return _size; }
+            set {
+                _memory = null;
+                _size = value;
+            }
+        }
+
+        public Process Process
+        {
+            get { return _process; }
+            set {
+                _memory = null;
+                _process = value;
+            }
+        }
+
+        public SignatureScanner(Process proc, IntPtr addr, int size)
+        {
+            if (proc == null)
+                throw new ArgumentException();
+            if (addr == IntPtr.Zero)
+                throw new ArgumentException();
+            if (size <= 0)
+                throw new ArgumentException();
+
+            _process = proc;
+            _address = addr;
+            _size = size;
+            _memory = new byte[1];
+        }
+
+        public IntPtr Scan(SigScanTarget target)
+        {
+            if (_memory == null || _memory.Length != _size)
+            {
+                _memory = new byte[_size];
+
+                byte[] bytes;
+
+                if (!_process.ReadBytes(_address, _size, out bytes))
+                {
+                    _memory = null;
+                    return IntPtr.Zero;
+                }
+
+                _memory = bytes;
+            }
+
+            foreach (SigScanTarget.Signature sig in target.Signatures)
+            {
+                IntPtr ptr = this.FindPattern(sig.Pattern, sig.Mask, sig.Offset);
+                if (ptr != IntPtr.Zero)
+                {
+                    if (target.OnFound != null)
+                        ptr = target.OnFound(_process, this, ptr);
+                    return ptr;
+                }
+            }
+
+            return IntPtr.Zero;
+        }
+
+        unsafe IntPtr FindPattern(byte[] sig, bool[] mask, int finalOffset)
+        {
+            if (sig.Length != mask.Length)
+                throw new ArgumentException();
+
+            fixed (byte* mem = _memory, s = sig)
+            fixed (bool* m = mask)
+            {
+                int sigLen = sig.Length;
+                int memLen = _memory.Length;
+
+                for (int addr = 0; addr < memLen; addr++)
+                {
+                    for (int i = 0; i < sigLen; i++)
+                    {
+                        if (m[i])
+                            continue;
+
+                        if (addr + i >= memLen || s[i] != mem[addr + i])
+                            goto next;
+                    }
+
+                    return _address + (addr + finalOffset);
+
+                next:
+                    ;
+                }
+            }
+
+            return IntPtr.Zero;
+        }
+    }
+
+    public class SigScanTarget
+    {
+        public struct Signature
+        {
+            public byte[] Pattern;
+            public bool[] Mask;
+            public int Offset;
+        }
+
+        public delegate IntPtr OnFoundCallback(Process proc, SignatureScanner scanner, IntPtr ptr);
+        public OnFoundCallback OnFound { get; set; }
+
+        private List<Signature> _sigs;
+        public ReadOnlyCollection<Signature> Signatures
+        {
+            get { return _sigs.AsReadOnly(); }
+        }
+
+        public SigScanTarget()
+        {
+            _sigs = new List<Signature>();
+        }
+
+        public SigScanTarget(int offset, params string[] signature)
+        {
+            _sigs = new List<Signature>();
+            this.AddSignature(offset, signature);
+        }
+
+        public SigScanTarget(int offset, byte[] binary)
+        {
+            _sigs = new List<Signature>();
+
+            var emptyMask = new bool[binary.Length];
+            _sigs.Add(new Signature { Pattern = binary, Mask = emptyMask, Offset = offset });
+        }
+
+        public void AddSignature(int offset, params string[] signature)
+        {
+            string sigStr = String.Join(String.Empty, signature).Replace(" ", String.Empty);
+            if (sigStr.Length % 2 != 0)
+                throw new ArgumentException();
+
+            var sigBytes = new List<byte>();
+            var sigMask = new List<bool>();
+
+            for (int i = 0; i < sigStr.Length; i += 2)
+            {
+                byte b;
+                if (Byte.TryParse(sigStr.Substring(i, 2), NumberStyles.HexNumber, null, out b))
+                {
+                    sigBytes.Add(b);
+                    sigMask.Add(false);
+                }
+                else
+                {
+                    sigBytes.Add(0);
+                    sigMask.Add(true);
+                }
+            }
+
+            _sigs.Add(new Signature {
+                Pattern = sigBytes.ToArray(),
+                Mask = sigMask.ToArray(),
+                Offset = offset
+            });
+        }
+    }
+}
