54c8456bb52e0c6f42a88059d4fc7c27330ba3af
Fatalis <fatalis@fatalis.pw>
2016-02-21 11:17:08 -0600

    ComponentUtil: Move p/invoke stuff to own class

--- LiveSplit/LiveSplit.Core/ComponentUtil/ProcessExtensions.cs
+++ LiveSplit/LiveSplit.Core/ComponentUtil/ProcessExtensions.cs
@@ -5,7 +5,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-
 #pragma warning disable 1591
 
 // Note: Please be careful when modifying this because it could break existing components!
@@ -39,90 +38,8 @@ namespace LiveSplit.ComponentUtil
         UTF16
     }
 
-    public enum MemPageState : uint
-    {
-        MEM_COMMIT = 0x1000,
-        MEM_RESERVE = 0x2000,
-        MEM_FREE = 0x10000,
-    }
-
-    public enum MemPageType : uint
-    {
-        MEM_PRIVATE = 0x20000,
-        MEM_MAPPED = 0x40000,
-        MEM_IMAGE = 0x1000000
-    }
-
-    [Flags]
-    public enum MemPageProtection : uint
-    {
-        PAGE_NOACCESS = 0x01,
-        PAGE_READONLY = 0x02,
-        PAGE_READWRITE = 0x04,
-        PAGE_WRITECOPY = 0x08,
-        PAGE_EXECUTE = 0x10,
-        PAGE_EXECUTE_READ = 0x20,
-        PAGE_EXECUTE_READWRITE = 0x40,
-        PAGE_EXECUTE_WRITECOPY = 0x80,
-        PAGE_GUARD = 0x100,
-        PAGE_NOCACHE = 0x200,
-        PAGE_WRITECOMBINE = 0x400,
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct MemoryBasicInformation // MEMORY_BASIC_INFORMATION
-    {
-        public IntPtr BaseAddress;
-        public IntPtr AllocationBase;
-        public MemPageProtection AllocationProtect;
-        public SizeT RegionSize;
-        public MemPageState State;
-        public MemPageProtection Protect;
-        public MemPageType Type;
-    }
-
     public static class ExtensionMethods
     {
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer,
-            SizeT dwSize, out SizeT lpNumberOfBytesRead);
-
-        [DllImport("psapi.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool EnumProcessModulesEx(IntPtr hProcess, [Out] IntPtr[] lphModule, uint cb,
-            out uint lpcbNeeded, uint dwFilterFlag);
-
-        [DllImport("psapi.dll", SetLastError = true)]
-        public static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName,
-            uint nSize);
-
-        [DllImport("psapi.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool GetModuleInformation(IntPtr hProcess, IntPtr hModule, [Out] out MODULEINFO lpmodinfo,
-            uint cb);
-
-        [DllImport("psapi.dll")]
-        public static extern uint GetModuleBaseName(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName,
-            uint nSize);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool IsWow64Process(IntPtr hProcess,
-            [Out, MarshalAs(UnmanagedType.Bool)] out bool wow64Process);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern SizeT VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress,
-            [Out] out MemoryBasicInformation lpBuffer, SizeT dwLength);
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MODULEINFO
-        {
-             public IntPtr lpBaseOfDll;
-             public uint SizeOfImage;
-             public IntPtr EntryPoint;
-        }
-
         private static Dictionary<int, ProcessModuleWow64Safe[]> ModuleCache = new Dictionary<int, ProcessModuleWow64Safe[]>();
 
         public static ProcessModuleWow64Safe MainModuleWow64Safe(this Process p)
@@ -143,7 +60,7 @@ namespace LiveSplit.ComponentUtil
             uint cb = (uint)IntPtr.Size*(uint)hModules.Length;
             uint cbNeeded;
 
-            if (!EnumProcessModulesEx(p.Handle, hModules, cb, out cbNeeded, LIST_MODULES_ALL))
+            if (!WinAPI.EnumProcessModulesEx(p.Handle, hModules, cb, out cbNeeded, LIST_MODULES_ALL))
                 throw new Win32Exception();
             uint numMods = cbNeeded / (uint)IntPtr.Size;
 
@@ -158,17 +75,17 @@ namespace LiveSplit.ComponentUtil
             for (int i = 0; i < numMods; i++)
             {
                 sb.Clear();
-                if (GetModuleFileNameEx(p.Handle, hModules[i], sb, (uint)sb.Capacity) == 0)
+                if (WinAPI.GetModuleFileNameEx(p.Handle, hModules[i], sb, (uint)sb.Capacity) == 0)
                     throw new Win32Exception();
                 string fileName = sb.ToString();
 
                 sb.Clear();
-                if (GetModuleBaseName(p.Handle, hModules[i], sb, (uint)sb.Capacity) == 0)
+                if (WinAPI.GetModuleBaseName(p.Handle, hModules[i], sb, (uint)sb.Capacity) == 0)
                     throw new Win32Exception();
                 string baseName = sb.ToString();
 
-                var moduleInfo = new MODULEINFO();
-                if (!GetModuleInformation(p.Handle, hModules[i], out moduleInfo, (uint)Marshal.SizeOf(moduleInfo)))
+                var moduleInfo = new WinAPI.MODULEINFO();
+                if (!WinAPI.GetModuleInformation(p.Handle, hModules[i], out moduleInfo, (uint)Marshal.SizeOf(moduleInfo)))
                     throw new Win32Exception();
 
                 ret.Add(new ProcessModuleWow64Safe()
@@ -200,7 +117,7 @@ namespace LiveSplit.ComponentUtil
             do
             {
                 MemoryBasicInformation mbi;
-                VirtualQueryEx(process.Handle, (IntPtr)addr, out mbi, mbiSize);
+                WinAPI.VirtualQueryEx(process.Handle, (IntPtr)addr, out mbi, mbiSize);
                 addr += (long)mbi.RegionSize;
 
                 // don't care about reserved/free pages
@@ -225,7 +142,7 @@ namespace LiveSplit.ComponentUtil
         public static bool Is64Bit(this Process process)
         {
             bool procWow64;
-            IsWow64Process(process.Handle, out procWow64);
+            WinAPI.IsWow64Process(process.Handle, out procWow64);
             if (Environment.Is64BitOperatingSystem && !procWow64)
                 return true;
             return false;
@@ -266,7 +183,7 @@ namespace LiveSplit.ComponentUtil
 
             SizeT read;
             val = null;
-            if (!ReadProcessMemory(process.Handle, addr, bytes, (SizeT)bytes.Length, out read)
+            if (!WinAPI.ReadProcessMemory(process.Handle, addr, bytes, (SizeT)bytes.Length, out read)
                 || read != (SizeT)bytes.Length)
                 return false;
 
@@ -286,7 +203,7 @@ namespace LiveSplit.ComponentUtil
 
             SizeT read;
             val = IntPtr.Zero;
-            if (!ReadProcessMemory(process.Handle, addr, bytes, (SizeT)bytes.Length, out read)
+            if (!WinAPI.ReadProcessMemory(process.Handle, addr, bytes, (SizeT)bytes.Length, out read)
                 || read != (SizeT)bytes.Length)
                 return false;
 
@@ -323,7 +240,7 @@ namespace LiveSplit.ComponentUtil
         {
             var bytes = new byte[sb.Capacity];
             SizeT read;
-            if (!ReadProcessMemory(process.Handle, addr, bytes, (SizeT)bytes.Length, out read)
+            if (!WinAPI.ReadProcessMemory(process.Handle, addr, bytes, (SizeT)bytes.Length, out read)
                 || read != (SizeT)bytes.Length)
                 return false;
 
