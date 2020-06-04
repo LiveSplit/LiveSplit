59e4ed40921f0748ad5a28806045d221ad403202
Voxelse <voxelse@gmail.com>
2020-06-05 01:46:49 +0200

    Add manual deref type for DeepPointer (#1864)
    * Add deref type for DeepPointer
    
    * Fix badly derived constructor
    
    * Apply suggestions from code review
    
    Co-authored-by: wooferzfg <spapushin@gmail.com>
    
    Co-authored-by: wooferzfg <spapushin@gmail.com>

--- LiveSplit/LiveSplit.Core/ComponentUtil/DeepPointer.cs
+++ LiveSplit/LiveSplit.Core/ComponentUtil/DeepPointer.cs
@@ -14,30 +14,44 @@ namespace LiveSplit.ComponentUtil
 
     public class DeepPointer
     {
+        public enum DerefType { Auto, Bit32, Bit64 }
+
         private IntPtr _absoluteBase;
         private bool _usingAbsoluteBase;
+        private DerefType _derefType;
 
         private OffsetT _base;
         private List<OffsetT> _offsets;
         private string _module;
-        
+
         public DeepPointer(IntPtr absoluteBase, params OffsetT[] offsets)
+            : this(absoluteBase, DerefType.Auto, offsets) { }
+
+        public DeepPointer(IntPtr absoluteBase, DerefType derefType, params OffsetT[] offsets)
         {
             _absoluteBase = absoluteBase;
             _usingAbsoluteBase = true;
+            _derefType = derefType;
 
             InitializeOffsets(offsets);
         }
 
         public DeepPointer(string module, OffsetT base_, params OffsetT[] offsets)
-            : this(base_, offsets)
+            : this(module, base_, DerefType.Auto, offsets) { }
+
+        public DeepPointer(string module, OffsetT base_, DerefType derefType, params OffsetT[] offsets)
+            : this(base_, derefType, offsets)
         {
             _module = module.ToLower();
         }
 
         public DeepPointer(OffsetT base_, params OffsetT[] offsets)
+            : this(base_, DerefType.Auto, offsets) { }
+
+        public DeepPointer(OffsetT base_, DerefType derefType, params OffsetT[] offsets)
         {
             _base = base_;
+            _derefType = derefType;
             InitializeOffsets(offsets);
         }
 
@@ -134,7 +148,11 @@ namespace LiveSplit.ComponentUtil
 
         public bool DerefOffsets(Process process, out IntPtr ptr)
         {
-            bool is64Bit = process.Is64Bit();
+            bool is64Bit;
+            if (_derefType == DerefType.Auto)
+                is64Bit = process.Is64Bit();
+            else
+                is64Bit = _derefType == DerefType.Bit64;
 
             if (!string.IsNullOrEmpty(_module))
             {
