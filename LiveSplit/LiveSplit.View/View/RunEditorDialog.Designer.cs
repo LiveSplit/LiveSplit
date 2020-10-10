c65a0f9e2c9d134f710cd2bccd9006017074dca5
Callum <31949934+HapyProgrammer@users.noreply.github.com>
2020-10-10 17:08:41 +1100

    Remove Enter key from closing the split editor (#2031)
    Removing the enter key from closing the split editor allows you to continue to edit your splits and prevents the crash.

--- LiveSplit/LiveSplit.View/View/RunEditorDialog.Designer.cs
+++ LiveSplit/LiveSplit.View/View/RunEditorDialog.Designer.cs
@@ -652,7 +652,6 @@
             // 
             // RunEditorDialog
             // 
-            this.AcceptButton = this.btnOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 511);
