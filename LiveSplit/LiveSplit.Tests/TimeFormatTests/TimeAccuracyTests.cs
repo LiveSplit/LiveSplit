ff3107472bcf7b42006fff7b63f054eb46494853
Roberto Alfonso <roberto.alfonso@praxisemr.com>
2021-01-09 23:54:49 -0300

    clean test
    * expected value at end of parameter list
    * object initializer for formatter
    * remove unnecessary null check for timespanText
    * use type inference for variables

--- LiveSplit/LiveSplit.Tests/TimeFormatTests/TimeAccuracyTests.cs
+++ LiveSplit/LiveSplit.Tests/TimeFormatTests/TimeAccuracyTests.cs
@@ -7,31 +7,30 @@ namespace LiveSplit.Tests.TimeFormatTests
     public class TimeAccuracyTests
     {
         [Theory]
-        [InlineData("00:00:00", "0:00", TimeAccuracy.Seconds)]
-        [InlineData("00:00:01", "0:01.0", TimeAccuracy.Tenths)]
-        [InlineData("00:00:02", "0:02.00", TimeAccuracy.Hundredths)]
-        [InlineData("00:00:03", "0:03.000", TimeAccuracy.Milliseconds)]
+        [InlineData("00:00:00", TimeAccuracy.Seconds, "0:00")]
+        [InlineData("00:00:01", TimeAccuracy.Tenths, "0:01.0")]
+        [InlineData("00:00:02", TimeAccuracy.Hundredths, "0:02.00")]
+        [InlineData("00:00:03", TimeAccuracy.Milliseconds, "0:03.000")]
 
-        [InlineData("00:00:04", "0:04", TimeAccuracy.Seconds)]
-        [InlineData("00:00:05.2", "0:05.2", TimeAccuracy.Tenths)]
-        [InlineData("00:00:06.22", "0:06.22", TimeAccuracy.Hundredths)]
-        [InlineData("00:00:07.222", "0:07.222", TimeAccuracy.Milliseconds)]
+        [InlineData("00:00:04", TimeAccuracy.Seconds, "0:04")]
+        [InlineData("00:00:05.2", TimeAccuracy.Tenths, "0:05.2")]
+        [InlineData("00:00:06.22", TimeAccuracy.Hundredths, "0:06.22")]
+        [InlineData("00:00:07.222", TimeAccuracy.Milliseconds, "0:07.222")]
 
-        [InlineData("00:00:08.8888888", "0:08", TimeAccuracy.Seconds)]
-        [InlineData("00:00:09.8888888", "0:09.8", TimeAccuracy.Tenths)]
-        [InlineData("00:00:10.8888888", "0:10.88", TimeAccuracy.Hundredths)]
-        [InlineData("00:00:11.8888888", "0:11.888", TimeAccuracy.Milliseconds)]
-        public void TestTimeAccuracy(string timespanText, string expected, TimeAccuracy accuracy)
+        [InlineData("00:00:08.8888888", TimeAccuracy.Seconds, "0:08")]
+        [InlineData("00:00:09.8888888", TimeAccuracy.Tenths, "0:09.8")]
+        [InlineData("00:00:10.8888888", TimeAccuracy.Hundredths, "0:10.88")]
+        [InlineData("00:00:11.8888888", TimeAccuracy.Milliseconds, "0:11.888")]
+        public void FormatsTimespanCorrectly_WhenDeterminedAccuracyIsGiven(string timespanText, TimeAccuracy accuracy, string expected)
         {
-            var formatter = new GeneralTimeFormatter();
-            formatter.DigitsFormat = DigitsFormat.SingleDigitMinutes;
-            formatter.Accuracy = accuracy;
+            var formatter = new GeneralTimeFormatter
+            {
+                DigitsFormat = DigitsFormat.SingleDigitMinutes,
+                Accuracy = accuracy
+            };
+            var time = TimeSpan.Parse(timespanText);
+            var formatted = formatter.Format(time);
 
-            TimeSpan? time = null;
-            if (timespanText != null)
-                time = TimeSpan.Parse(timespanText);
-
-            string formatted = formatter.Format(time);
             Assert.Equal(expected, formatted);
         }
     }
