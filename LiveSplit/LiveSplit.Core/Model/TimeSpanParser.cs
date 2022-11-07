4becd442f8f44b57e5c09df7a04cff741d7e8900
wooferzfg <spapushin@gmail.com>
2022-11-07 13:06:39 -0500

    Don't parse TimeSpans as floats (#2287)
    * Don't parse TimeSpans as floats
    
    * PR feedback

--- LiveSplit/LiveSplit.Core/Model/TimeSpanParser.cs
+++ LiveSplit/LiveSplit.Core/Model/TimeSpanParser.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Globalization;
 using LiveSplit.TimeFormatters;
+using System.Collections.Generic;
 
 namespace LiveSplit.Model
 {
@@ -25,12 +26,48 @@ namespace LiveSplit.Model
                 timeString = timeString.Substring(1);
             }
 
-            var seconds = timeString
+            var splitTimeString = timeString.Split(new char[] {'.'}, 2);
+            var secondsText = splitTimeString[0];
+            var secondsTicks = ParseSecondsAsTicks(secondsText);
+
+            var fractionTicks = 0UL;
+            if (splitTimeString.Length > 1)
+            {
+                var fractionText = splitTimeString[1];
+                fractionTicks = ParseFractionAsTicks(fractionText);
+            }
+
+            return TimeSpan.FromTicks(factor * (long)(secondsTicks + fractionTicks));
+        }
+
+        private static ulong ParseFractionAsTicks(string fractionText)
+        {
+            if (fractionText.Length > 7)
+                fractionText = fractionText.Substring(0, 7);
+
+            return ulong.Parse(fractionText, NumberStyles.Integer, CultureInfo.InvariantCulture) * powersOfTen[7 - fractionText.Length];
+        }
+
+        private static ulong ParseSecondsAsTicks(string secondsText)
+        {
+            var totalSeconds = secondsText
                 .Split(':')
-                .Select(x => double.Parse(x, NumberStyles.Float, CultureInfo.InvariantCulture))
+                .Select(x => ulong.Parse(x, NumberStyles.Integer, CultureInfo.InvariantCulture))
                 .Aggregate((a, b) => 60 * a + b);
 
-            return TimeSpan.FromTicks((long)(factor * seconds * TimeSpan.TicksPerSecond));
+            return totalSeconds * TimeSpan.TicksPerSecond;
         }
+
+        private static readonly ulong[] powersOfTen =
+        {
+            1UL,
+            10UL,
+            100UL,
+            1000UL,
+            10000UL,
+            100000UL,
+            1000000UL,
+            10000000UL,
+        };
     }
 }
