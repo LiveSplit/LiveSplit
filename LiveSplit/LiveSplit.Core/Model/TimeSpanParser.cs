d000b72d7ca1fabae5cc70706740ca885d941f1a
wooferzfg <spapushin@gmail.com>
2022-11-06 20:34:22 -0500

    Don't parse TimeSpans as floats

--- LiveSplit/LiveSplit.Core/Model/TimeSpanParser.cs
+++ LiveSplit/LiveSplit.Core/Model/TimeSpanParser.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Globalization;
 using LiveSplit.TimeFormatters;
+using System.Collections.Generic;
 
 namespace LiveSplit.Model
 {
@@ -25,12 +26,48 @@ namespace LiveSplit.Model
                 timeString = timeString.Substring(1);
             }
 
-            var seconds = timeString
+            var splitTimeString = timeString.Split(new char[] {'.'}, 2);
+            var secondsText = splitTimeString[0];
+            var secondsTicks = ParseSecondsAsTicks(secondsText);
+
+            var fractionTicks = 0L;
+            if (splitTimeString.Length > 1)
+            {
+                var fractionText = splitTimeString[1];
+                fractionTicks = ParseFractionAsTicks(fractionText);
+            }
+
+            return TimeSpan.FromTicks((long)(factor * (secondsTicks + fractionTicks)));
+        }
+
+        private static long ParseFractionAsTicks(string fractionText)
+        {
+            if (fractionText.Length > 7)
+                fractionText = fractionText.Substring(0, 7);
+
+            return long.Parse(fractionText) * powersOfTen[7 - fractionText.Length];
+        }
+
+        private static long ParseSecondsAsTicks(string secondsText)
+        {
+            var totalSeconds = secondsText
                 .Split(':')
-                .Select(x => double.Parse(x, NumberStyles.Float, CultureInfo.InvariantCulture))
+                .Select(x => long.Parse(x, NumberStyles.Float, CultureInfo.InvariantCulture))
                 .Aggregate((a, b) => 60 * a + b);
 
-            return TimeSpan.FromTicks((long)(factor * seconds * TimeSpan.TicksPerSecond));
+            return totalSeconds * TimeSpan.TicksPerSecond;
         }
+
+        private static readonly Dictionary<int, long> powersOfTen = new Dictionary<int, long>()
+        {
+            { 0, 1L },
+            { 1, 10L },
+            { 2, 100L },
+            { 3, 1000L },
+            { 4, 10000L },
+            { 5, 100000L },
+            { 6, 1000000L },
+            { 7, 10000000L },
+        };
     }
 }
