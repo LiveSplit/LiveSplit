50b578f951b856ad23a30fec0e1ccf8aa1ea15c8
rpgrca <rpgrca@gmail.com>
2023-01-22 00:27:18 -0300

    add tests for Attempt struct constructor

---
+++ LiveSplit/LiveSplit.Tests/Model/AttemptMust.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using LiveSplit.Model;
+
+namespace LiveSplit.Tests.Model
+{
+    public class AttemptMust
+    {
+        private static readonly DateTime AnyDateTime = new DateTime(2020, 12, 13);
+
+        [Theory]
+        [MemberData(nameof(AttemptConstructorFeeder))]
+        public void BeInitializedCorrectly(AtomicDateTime? anyStart, AtomicDateTime? anyEnd, TimeSpan? anyPause)
+        {
+            var anyIndex = 0;
+            var anyTime = Time.Zero;
+
+            var sut = new Attempt(anyIndex, anyTime, anyStart, anyEnd, anyPause);
+            Assert.Equal(anyIndex, sut.Index);
+            Assert.Equal(anyTime, sut.Time);
+            Assert.Equal(anyStart, sut.Started);
+            Assert.Equal(anyEnd, sut.Ended);
+            Assert.Equal(anyPause, sut.PauseTime);
+        }
+
+        public static IEnumerable<object[]> AttemptConstructorFeeder()
+        {
+            object[] possibleValues = { null, new AtomicDateTime(AnyDateTime, false) };
+            foreach (var anyStart in possibleValues)
+            {
+                foreach (var anyEnd in possibleValues)
+                {
+                    foreach (var anyPause in new object[] { null, TimeSpan.Zero })
+                    {
+                        yield return new object[] { anyStart, anyEnd, anyPause };
+                    }
+                }
+            }
+        }
+    }
+}
