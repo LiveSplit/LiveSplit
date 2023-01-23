f86b7f1a5d56c3cc81791bc4c58a5b3d556fb76c
rpgrca <rpgrca@gmail.com>
2023-01-23 11:52:12 -0300

    add tests for IndexedTime

---
+++ LiveSplit/LiveSplit.Tests/Model/IndexedTimeMust.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+using LiveSplit.Model;
+using static LiveSplit.Tests.Model.Constants;
+
+namespace LiveSplit.Tests.Model
+{
+    public class IndexedTimeMust
+    {
+        [Fact]
+        public void BeInitializedCorrectly_WhenUsingDefaultConstructor()
+        {
+            var sut = new IndexedTime();
+            Assert.Equal(default, sut.Time);
+            Assert.Equal(default, sut.Index);
+        }
+
+        [Fact]
+        public void BeInitializedCorrectly_WhenAssigningValuesInConstructor()
+        {
+            var anyTime = new Time(TimeSpan.FromTicks(AnyTickValue), TimeSpan.FromTicks(AnyTickValue));
+            var anyIndex = 1;
+            var sut = new IndexedTime(anyTime, anyIndex);
+            Assert.Equal(anyIndex, sut.Index);
+            Assert.Equal(anyTime, sut.Time);
+        }
+    }
+}
