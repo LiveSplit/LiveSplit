838df70e7182a241cda426f224b43d3a24b7ba10
rpgrca <rpgrca@gmail.com>
2023-01-23 23:35:00 -0300

    add tests for IndexedTimeHelper

--- LiveSplit/LiveSplit.Tests/Model/Constants.cs
+++ LiveSplit/LiveSplit.Tests/Model/Constants.cs
@@ -8,10 +8,11 @@ namespace LiveSplit.Tests.Model
         public static readonly DateTime AnotherDateTime = new DateTime(2020, 12, 4);
         public const long DateTimeDifference = 7776000000000;
         public const long AnyTickValue = 7776000000000;
+        public const long YetAnotherTickValue = 7760000000000;
         public const string TimeSerializationAsString = "9.00:00:00 | 03:00:00";
         public static readonly TimeSpan AnyTimeSpan = TimeSpan.FromTicks(AnyTickValue);
         public static readonly TimeSpan AnotherTimeSpan = TimeSpan.FromHours(3);
-        public static readonly TimeSpan YetAnotherTimeSpan = TimeSpan.FromMinutes(1774);
+        public static readonly TimeSpan YetAnotherTimeSpan = TimeSpan.FromTicks(YetAnotherTickValue);
         public const long Difference = 7668000000000;
         public const long Addition = 7884000000000;
     }
