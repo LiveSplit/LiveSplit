7c7606e0f7fd3c5f54c50b83b3f293967b80c817
wooferzfg <spapushin@gmail.com>
2024-03-31 13:53:51 -0400

    Add Racetime tokens to web credentials
    Fix #2374

--- LiveSplit/LiveSplit.Core/Web/WebCredentials.cs
+++ LiveSplit/LiveSplit.Core/Web/WebCredentials.cs
@@ -11,6 +11,8 @@ namespace LiveSplit.Web
         private const string Twitch = "LiveSplit_TwitchAccessToken";
         private const string SpeedrunCom = "LiveSplit_SpeedrunComAccessToken";
         private const string SpeedRunsLive = "LiveSplit_SpeedRunsLiveIRC";
+        private const string RacetimeAccess = "LiveSplit_racetimegg_accesstoken";
+        private const string RacetimeRefresh = "LiveSplit_racetimegg_refreshtoken";
 
         public static string TwitchAccessToken
         {
@@ -31,19 +33,33 @@ namespace LiveSplit.Web
             }
             set { CredentialManager.WriteCredential(SpeedRunsLive, value.Username, value.Password); }
         }
+        public static string RacetimeAccessToken
+        {
+            get { return CredentialManager.ReadCredential(RacetimeAccess)?.Password; }
+            set { CredentialManager.WriteCredential(RacetimeAccess, "", value); }
+        }
+        public static string RacetimeRefreshToken
+        {
+            get { return CredentialManager.ReadCredential(RacetimeRefresh)?.Password; }
+            set { CredentialManager.WriteCredential(RacetimeRefresh, "", value); }
+        }
 
         public static void DeleteAllCredentials()
         {
             CredentialManager.DeleteCredential(Twitch);
             CredentialManager.DeleteCredential(SpeedrunCom);
             CredentialManager.DeleteCredential(SpeedRunsLive);
+            CredentialManager.DeleteCredential(RacetimeAccess);
+            CredentialManager.DeleteCredential(RacetimeRefresh);
         }
 
         public static bool AnyCredentialsExist()
         {
             return CredentialManager.CredentialExists(Twitch)
                 || CredentialManager.CredentialExists(SpeedrunCom)
-                || CredentialManager.CredentialExists(SpeedRunsLive);
+                || CredentialManager.CredentialExists(SpeedRunsLive)
+                || CredentialManager.CredentialExists(RacetimeAccess)
+                || CredentialManager.CredentialExists(RacetimeRefresh);
         }
     }
 
