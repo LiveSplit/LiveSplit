9d548c235601305a2112140728600365b5a07436
Yummy_Bacon5 <68166338+YummyBacon5@users.noreply.github.com>
2024-04-08 23:31:09 +0100

    Update SRC API key link to not include their name (#2444)
    * Update SRC API key link
    
    * Update SpeedrunComAPIKeyPrompt.cs
    
    ---------
    
    Co-authored-by: wooferzfg <spapushin@gmail.com>

--- LiveSplit/LiveSplit.Core/Web/Share/SpeedrunComAPIKeyPrompt.cs
+++ LiveSplit/LiveSplit.Core/Web/Share/SpeedrunComAPIKeyPrompt.cs
@@ -12,10 +12,10 @@ namespace LiveSplit.Web.Share
     {
         public string GetAccessToken()
         {
-            Process.Start("https://www.speedrun.com");
+            Process.Start("https://www.speedrun.com/settings/api");
 
             string accessToken = null;
-            InputBox.Show("Speedrun.com Authentication", "Enter your Speedrun.com API Key (Go to speedrun.com/users/<username>/settings/api):", ref accessToken);
+            InputBox.Show("Speedrun.com Authentication", "Enter your Speedrun.com API Key:", ref accessToken);
             return accessToken;
         }
     }
