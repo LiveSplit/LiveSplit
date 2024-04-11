6b86c7812a0fed18f7ef8102971643d694d06d08
Ero <imjustero@gmail.com>
2024-04-11 18:43:14 +0200

    Update relative paths, sync submodules

--- src/LiveSplit.Core/build_livesplit_core.sh
+++ src/LiveSplit.Core/build_livesplit_core.sh
@@ -1,8 +1,8 @@
-cd ../../LiveSplit/Libs/livesplit-core
-RUSTFLAGS="-C target-feature=+crt-static" cargo +stable-x86_64-pc-windows-msvc rustc --release -p livesplit-core-capi --crate-type cdylib --target-dir ../../../src/LiveSplit.Core/x64/target
-cp ../../../src/LiveSplit.Core/x64/target/release/livesplit_core.dll ../../../src/LiveSplit.Core/x64/.
-RUSTFLAGS="-C target-feature=+crt-static" cargo +stable-i686-pc-windows-msvc rustc --release -p livesplit-core-capi --crate-type cdylib --target-dir ../../../src/LiveSplit.Core/x86/target
-cp ../../../src/LiveSplit.Core/x86/target/release/livesplit_core.dll ../../../src/LiveSplit.Core/x86/.
+cd ../../lib/livesplit-core
+RUSTFLAGS="-C target-feature=+crt-static" cargo +stable-x86_64-pc-windows-msvc rustc --release -p livesplit-core-capi --crate-type cdylib --target-dir ../../src/LiveSplit.Core/x64/target
+cp ../../src/LiveSplit.Core/x64/target/release/livesplit_core.dll ../../src/LiveSplit.Core/x64/.
+RUSTFLAGS="-C target-feature=+crt-static" cargo +stable-i686-pc-windows-msvc rustc --release -p livesplit-core-capi --crate-type cdylib --target-dir ../../src/LiveSplit.Core/x86/target
+cp ../../src/LiveSplit.Core/x86/target/release/livesplit_core.dll ../../src/LiveSplit.Core/x86/.
 cd capi/bind_gen
 cargo run
-cp ../bindings/LiveSplitCore.cs ../../../../../src/LiveSplit.Core/.
+cp ../bindings/LiveSplitCore.cs ../../../../src/LiveSplit.Core/.
