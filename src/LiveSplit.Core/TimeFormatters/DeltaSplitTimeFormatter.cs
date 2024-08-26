858d632bd82688c5057462a6e7ea3fa561bc16ca
10_days_till_xmas <159474213+10-days-till-xmas@users.noreply.github.com>
2024-08-26 01:26:19 +0100

    Move DeltaSplitTimeFormatter.cs
    Moved from LiveSplit.Splits and LiveSplit.Subsplits

---
+++ src/LiveSplit.Core/TimeFormatters/DeltaSplitTimeFormatter.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LiveSplit.TimeFormatters
+{
+    public class DeltaSplitTimeFormatter : GeneralTimeFormatter
+    {
+        public DeltaSplitTimeFormatter(TimeAccuracy accuracy, bool dropDecimals)
+        {
+            Accuracy = accuracy;
+            DropDecimals = dropDecimals;
+            NullFormat = NullFormat.Dash;
+            ShowPlus = true;
+        }
+    }
+}
