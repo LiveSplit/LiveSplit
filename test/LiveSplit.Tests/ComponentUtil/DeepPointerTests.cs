116bde059d8e4d417f541457425d9e4258e2f9e7
Ero <imjustero@gmail.com>
2024-09-02 16:29:11 +0200

    Fix tests

--- test/LiveSplit.Tests/ComponentUtil/DeepPointerTests.cs
+++ test/LiveSplit.Tests/ComponentUtil/DeepPointerTests.cs
@@ -15,20 +15,20 @@ public class DeepPointerTests
     [Fact]
     public void ThrowException_WhenInitializedWithValidBaseAndNullOffsets()
     {
-        Assert.Throws<ArgumentNullException>(() => new DeepPointer(0, null));
+        Assert.Throws<NullReferenceException>(() => new DeepPointer(0, null));
     }
 
     [Fact]
     public void ThrowException_WhenInitializedWithValidModuleAndBaseAndNullOffsets()
     {
-        Assert.Throws<ArgumentNullException>(() => new DeepPointer(string.Empty, 0, null));
+        Assert.Throws<NullReferenceException>(() => new DeepPointer(string.Empty, 0, null));
     }
 
     [Theory]
     [MemberData(nameof(DerefTypeFeeder))]
     public void ThrowException_WhenInitializedWithValidBaseAndDerefTypeButNullOffsets(DerefType anyDerefType)
     {
-        Assert.Throws<ArgumentNullException>(() => new DeepPointer(0, anyDerefType, null));
+        Assert.Throws<NullReferenceException>(() => new DeepPointer(0, anyDerefType, null));
     }
 
     public static IEnumerable<object[]> DerefTypeFeeder()
@@ -42,18 +42,18 @@ public class DeepPointerTests
     [Fact]
     public void ThrowException_WhenInitializedWithValidModuleBaseDerefTypeButNullOffsets()
     {
-        Assert.Throws<ArgumentNullException>(() => new DeepPointer(string.Empty, 0, DerefType.Auto, null));
+        Assert.Throws<NullReferenceException>(() => new DeepPointer(string.Empty, 0, DerefType.Auto, null));
     }
 
     [Fact]
     public void ThrowException_WhenInitializedWithValidPointerAndNullOffsets()
     {
-        Assert.Throws<ArgumentNullException>(() => new DeepPointer((nint)0, null));
+        Assert.Throws<NullReferenceException>(() => new DeepPointer((nint)0, null));
     }
 
     [Fact]
     public void ThrowException_WhenInitializedWithValidPointerDerefTypeButNullOffset()
     {
-        Assert.Throws<ArgumentNullException>(() => new DeepPointer((nint)0, DerefType.Auto, null));
+        Assert.Throws<NullReferenceException>(() => new DeepPointer((nint)0, DerefType.Auto, null));
     }
 }
