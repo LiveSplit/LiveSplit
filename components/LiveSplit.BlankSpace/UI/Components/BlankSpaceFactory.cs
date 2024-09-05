1a365e5d8b00c988831086136e6a7a753f90cf0e
Ero <imjustero@gmail.com>
2024-09-05 14:06:08 +0200

    Move `LiveSplit.BlankSpace`

---
+++ components/LiveSplit.BlankSpace/UI/Components/BlankSpaceFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using LiveSplit.Model;
+using LiveSplit.UI.Components;
+
+[assembly: ComponentFactory(typeof(BlankSpaceFactory))]
+
+namespace LiveSplit.UI.Components;
+
+public class BlankSpaceFactory : IComponentFactory
+{
+    public string ComponentName => "Blank Space";
+
+    public string Description => "Shows empty space with a configurable size.";
+
+    public ComponentCategory Category => ComponentCategory.Other;
+
+    public IComponent Create(LiveSplitState state)
+    {
+        return new BlankSpace();
+    }
+
+    public string UpdateName => ComponentName;
+
+    public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.BlankSpace.xml";
+
+    public string UpdateURL => "http://livesplit.org/update/";
+
+    public Version Version => Version.Parse("1.8.29");
+}
---
+++ components/LiveSplit.BlankSpace/UI/Components/BlankSpaceSettings.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace LiveSplit.UI.Components
+{
+    partial class BlankSpaceSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.trkSize = new System.Windows.Forms.TrackBar();
+            this.btnColor1 = new System.Windows.Forms.Button();
+            this.btnColor2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.cmbGradientType = new System.Windows.Forms.ComboBox();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trkSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 157F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 95F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.trkSize, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnColor1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnColor2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label11, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cmbGradientType, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblSize, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(7, 7);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(462, 58);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // trkSize
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.trkSize, 4);
+            this.trkSize.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trkSize.Location = new System.Drawing.Point(160, 32);
+            this.trkSize.Name = "trkSize";
+            this.trkSize.Size = new System.Drawing.Size(299, 23);
+            this.trkSize.TabIndex = 4;
+            this.trkSize.TickStyle = System.Windows.Forms.TickStyle.None;
+            // 
+            // btnColor1
+            // 
+            this.btnColor1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnColor1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnColor1.Location = new System.Drawing.Point(160, 3);
+            this.btnColor1.Name = "btnColor1";
+            this.btnColor1.Size = new System.Drawing.Size(23, 23);
+            this.btnColor1.TabIndex = 0;
+            this.btnColor1.UseVisualStyleBackColor = false;
+            this.btnColor1.Click += new System.EventHandler(this.ColorButtonClick);
+            // 
+            // btnColor2
+            // 
+            this.btnColor2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnColor2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnColor2.Location = new System.Drawing.Point(189, 3);
+            this.btnColor2.Name = "btnColor2";
+            this.btnColor2.Size = new System.Drawing.Size(23, 23);
+            this.btnColor2.TabIndex = 1;
+            this.btnColor2.UseVisualStyleBackColor = false;
+            this.btnColor2.Click += new System.EventHandler(this.ColorButtonClick);
+            // 
+            // label11
+            // 
+            this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(3, 8);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(151, 13);
+            this.label11.TabIndex = 39;
+            this.label11.Text = "Background Color:";
+            // 
+            // cmbGradientType
+            // 
+            this.cmbGradientType.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.cmbGradientType, 2);
+            this.cmbGradientType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGradientType.FormattingEnabled = true;
+            this.cmbGradientType.Items.AddRange(new object[] {
+            "Plain",
+            "Vertical",
+            "Horizontal"});
+            this.cmbGradientType.Location = new System.Drawing.Point(218, 4);
+            this.cmbGradientType.Name = "cmbGradientType";
+            this.cmbGradientType.Size = new System.Drawing.Size(241, 21);
+            this.cmbGradientType.TabIndex = 2;
+            this.cmbGradientType.SelectedIndexChanged += new System.EventHandler(this.cmbGradientType_SelectedIndexChanged);
+            // 
+            // lblSize
+            // 
+            this.lblSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(3, 37);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(151, 13);
+            this.lblSize.TabIndex = 1;
+            this.lblSize.Text = "Height:";
+            // 
+            // BlankSpaceSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "BlankSpaceSettings";
+            this.Padding = new System.Windows.Forms.Padding(7);
+            this.Size = new System.Drawing.Size(476, 72);
+            this.Load += new System.EventHandler(this.BlankSpaceSettings_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trkSize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TrackBar trkSize;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.ComboBox cmbGradientType;
+        private System.Windows.Forms.Button btnColor1;
+        private System.Windows.Forms.Button btnColor2;
+        private System.Windows.Forms.Label label11;
+    }
+}
