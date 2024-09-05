5370843d00e4f193e88fc14daea43ba0709c7854
Ero <imjustero@gmail.com>
2024-09-05 15:56:39 +0200

    Move `LiveSplit.TotalPlaytime`

---
+++ components/LiveSplit.TotalPlaytime/UI/Components/TotalPlaytimeComponent.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Xml;
+
+using LiveSplit.Model;
+using LiveSplit.TimeFormatters;
+
+namespace LiveSplit.UI.Components;
+
+public class TotalPlaytimeComponent : IComponent
+{
+    protected ITimeFormatter HoursTimeFormatter { get; set; }
+    protected ITimeFormatter DaysTimeFormatter { get; set; }
+    protected InfoTimeComponent InternalComponent { get; set; }
+    protected TotalPlaytimeSettings Settings { get; set; }
+
+    protected TimerPhase LastPhase { get; set; }
+    protected int LastAttemptCount { get; set; }
+    protected IRun LastRun { get; set; }
+
+    public string ComponentName => "Total Playtime";
+
+    public IDictionary<string, Action> ContextMenuControls => null;
+
+    public float HorizontalWidth => InternalComponent.HorizontalWidth;
+
+    public float VerticalHeight => InternalComponent.VerticalHeight;
+
+    public float MinimumHeight => InternalComponent.MinimumHeight;
+
+    public float MinimumWidth => InternalComponent.MinimumWidth;
+
+    public float PaddingBottom => InternalComponent.PaddingBottom;
+
+    public float PaddingLeft => InternalComponent.PaddingLeft;
+
+    public float PaddingRight => InternalComponent.PaddingRight;
+
+    public float PaddingTop => InternalComponent.PaddingTop;
+
+    public TotalPlaytimeComponent(LiveSplitState state)
+    {
+        HoursTimeFormatter = new RegularTimeFormatter();
+        DaysTimeFormatter = new DaysTimeFormatter();
+        InternalComponent = new InfoTimeComponent("Total Playtime", TimeSpan.Zero, DaysTimeFormatter);
+        Settings = new TotalPlaytimeSettings()
+        {
+            CurrentState = state
+        };
+    }
+
+    private void DrawBackground(Graphics g, LiveSplitState state, float width, float height)
+    {
+        if (Settings.BackgroundColor.A > 0
+            || (Settings.BackgroundGradient != GradientType.Plain
+            && Settings.BackgroundColor2.A > 0))
+        {
+            var gradientBrush = new LinearGradientBrush(
+                        new PointF(0, 0),
+                        Settings.BackgroundGradient == GradientType.Horizontal
+                        ? new PointF(width, 0)
+                        : new PointF(0, height),
+                        Settings.BackgroundColor,
+                        Settings.BackgroundGradient == GradientType.Plain
+                        ? Settings.BackgroundColor
+                        : Settings.BackgroundColor2);
+            g.FillRectangle(gradientBrush, 0, 0, width, height);
+        }
+    }
+
+    public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+    {
+        DrawBackground(g, state, HorizontalWidth, height);
+
+        InternalComponent.NameLabel.HasShadow
+            = InternalComponent.ValueLabel.HasShadow
+            = state.LayoutSettings.DropShadows;
+
+        InternalComponent.NameLabel.ForeColor = Settings.OverrideTextColor ? Settings.TextColor : state.LayoutSettings.TextColor;
+        InternalComponent.ValueLabel.ForeColor = Settings.OverrideTimeColor ? Settings.TimeColor : state.LayoutSettings.TextColor;
+
+        InternalComponent.DrawHorizontal(g, state, height, clipRegion);
+    }
+
+    public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
+    {
+        DrawBackground(g, state, width, VerticalHeight);
+
+        InternalComponent.DisplayTwoRows = Settings.Display2Rows;
+
+        InternalComponent.NameLabel.HasShadow
+            = InternalComponent.ValueLabel.HasShadow
+            = state.LayoutSettings.DropShadows;
+
+        InternalComponent.NameLabel.ForeColor = Settings.OverrideTextColor ? Settings.TextColor : state.LayoutSettings.TextColor;
+        InternalComponent.ValueLabel.ForeColor = Settings.OverrideTimeColor ? Settings.TimeColor : state.LayoutSettings.TextColor;
+
+        InternalComponent.DrawVertical(g, state, width, clipRegion);
+    }
+
+    public XmlNode GetSettings(XmlDocument document)
+    {
+        return Settings.GetSettings(document);
+    }
+
+    public Control GetSettingsControl(LayoutMode mode)
+    {
+        Settings.Mode = mode;
+        return Settings;
+    }
+
+    public void SetSettings(XmlNode settings)
+    {
+        Settings.SetSettings(settings);
+    }
+
+    public TimeSpan CalculateTotalPlaytime(LiveSplitState state)
+    {
+        TimeSpan totalPlaytime = TimeSpan.Zero;
+
+        foreach (Attempt attempt in state.Run.AttemptHistory)
+        {
+            TimeSpan? duration = attempt.Duration;
+
+            if (duration.HasValue)
+            {
+                //Either >= 1.6.0 or a finished run
+                totalPlaytime += duration.Value - (attempt.PauseTime ?? TimeSpan.Zero);
+            }
+            else
+            {
+                //Must be < 1.6.0 and a reset
+                //Calculate the sum of the segments for that run
+
+                foreach (ISegment segment in state.Run)
+                {
+                    if (segment.SegmentHistory.TryGetValue(attempt.Index, out Time segmentHistoryElement) && segmentHistoryElement.RealTime.HasValue)
+                    {
+                        totalPlaytime += segmentHistoryElement.RealTime.Value;
+                    }
+                }
+            }
+        }
+
+        totalPlaytime += state.CurrentAttemptDuration - (state.PauseTime ?? TimeSpan.Zero);
+
+        return totalPlaytime;
+    }
+
+    public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+    {
+        InternalComponent.Formatter = Settings.ShowTotalHours ? HoursTimeFormatter : DaysTimeFormatter;
+
+        if (LastAttemptCount != state.Run.AttemptHistory.Count
+            || LastPhase != state.CurrentPhase
+            || LastRun != state.Run
+            || state.CurrentPhase == TimerPhase.Running
+            || state.CurrentPhase == TimerPhase.Paused)
+        {
+            InternalComponent.TimeValue = CalculateTotalPlaytime(state);
+
+            LastAttemptCount = state.Run.AttemptHistory.Count;
+            LastPhase = state.CurrentPhase;
+            LastRun = state.Run;
+        }
+
+        InternalComponent.Update(invalidator, state, width, height, mode);
+    }
+
+    public void Dispose()
+    {
+        InternalComponent.Dispose();
+    }
+
+    public int GetSettingsHashCode()
+    {
+        return Settings.GetSettingsHashCode();
+    }
+}
---
+++ components/LiveSplit.TotalPlaytime/UI/Components/TotalPlaytimeSettings.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml;
+
+using LiveSplit.Model;
+
+namespace LiveSplit.UI.Components;
+
+public partial class TotalPlaytimeSettings : UserControl
+{
+    public Color TextColor { get; set; }
+    public bool OverrideTextColor { get; set; }
+    public Color TimeColor { get; set; }
+    public bool OverrideTimeColor { get; set; }
+
+    public Color BackgroundColor { get; set; }
+    public Color BackgroundColor2 { get; set; }
+    public GradientType BackgroundGradient { get; set; }
+    public string GradientString
+    {
+        get => BackgroundGradient.ToString();
+        set => BackgroundGradient = (GradientType)Enum.Parse(typeof(GradientType), value);
+    }
+
+    public LiveSplitState CurrentState { get; set; }
+    public bool Display2Rows { get; set; }
+    public bool ShowTotalHours { get; set; }
+
+    public LayoutMode Mode { get; set; }
+
+    public TotalPlaytimeSettings()
+    {
+        InitializeComponent();
+
+        TextColor = Color.FromArgb(255, 255, 255);
+        OverrideTextColor = false;
+        TimeColor = Color.FromArgb(255, 255, 255);
+        OverrideTimeColor = false;
+        BackgroundColor = Color.Transparent;
+        BackgroundColor2 = Color.Transparent;
+        BackgroundGradient = GradientType.Plain;
+        Display2Rows = false;
+        ShowTotalHours = false;
+
+        chkOverrideTextColor.DataBindings.Add("Checked", this, "OverrideTextColor", false, DataSourceUpdateMode.OnPropertyChanged);
+        btnTextColor.DataBindings.Add("BackColor", this, "TextColor", false, DataSourceUpdateMode.OnPropertyChanged);
+        chkOverrideTimeColor.DataBindings.Add("Checked", this, "OverrideTimeColor", false, DataSourceUpdateMode.OnPropertyChanged);
+        btnTimeColor.DataBindings.Add("BackColor", this, "TimeColor", false, DataSourceUpdateMode.OnPropertyChanged);
+        cmbGradientType.DataBindings.Add("SelectedItem", this, "GradientString", false, DataSourceUpdateMode.OnPropertyChanged);
+        btnColor1.DataBindings.Add("BackColor", this, "BackgroundColor", false, DataSourceUpdateMode.OnPropertyChanged);
+        btnColor2.DataBindings.Add("BackColor", this, "BackgroundColor2", false, DataSourceUpdateMode.OnPropertyChanged);
+        chkShowTotalHours.DataBindings.Add("Checked", this, "ShowTotalHours", false, DataSourceUpdateMode.OnPropertyChanged);
+    }
+
+    private void chkOverrideTimeColor_CheckedChanged(object sender, EventArgs e)
+    {
+        label2.Enabled = btnTimeColor.Enabled = chkOverrideTimeColor.Checked;
+    }
+
+    private void chkOverrideTextColor_CheckedChanged(object sender, EventArgs e)
+    {
+        label1.Enabled = btnTextColor.Enabled = chkOverrideTextColor.Checked;
+    }
+
+    private void TotalPlaytimeSettings_Load(object sender, EventArgs e)
+    {
+        chkOverrideTextColor_CheckedChanged(null, null);
+        chkOverrideTimeColor_CheckedChanged(null, null);
+        if (Mode == LayoutMode.Horizontal)
+        {
+            chkTwoRows.Enabled = false;
+            chkTwoRows.DataBindings.Clear();
+            chkTwoRows.Checked = true;
+        }
+        else
+        {
+            chkTwoRows.Enabled = true;
+            chkTwoRows.DataBindings.Clear();
+            chkTwoRows.DataBindings.Add("Checked", this, "Display2Rows", false, DataSourceUpdateMode.OnPropertyChanged);
+        }
+    }
+
+    private void cmbGradientType_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        btnColor1.Visible = cmbGradientType.SelectedItem.ToString() != "Plain";
+        btnColor2.DataBindings.Clear();
+        btnColor2.DataBindings.Add("BackColor", this, btnColor1.Visible ? "BackgroundColor2" : "BackgroundColor", false, DataSourceUpdateMode.OnPropertyChanged);
+        GradientString = cmbGradientType.SelectedItem.ToString();
+    }
+
+    public void SetSettings(XmlNode node)
+    {
+        var element = (XmlElement)node;
+        TextColor = SettingsHelper.ParseColor(element["TextColor"]);
+        OverrideTextColor = SettingsHelper.ParseBool(element["OverrideTextColor"]);
+        TimeColor = SettingsHelper.ParseColor(element["TimeColor"]);
+        OverrideTimeColor = SettingsHelper.ParseBool(element["OverrideTimeColor"]);
+        BackgroundColor = SettingsHelper.ParseColor(element["BackgroundColor"]);
+        BackgroundColor2 = SettingsHelper.ParseColor(element["BackgroundColor2"]);
+        GradientString = SettingsHelper.ParseString(element["BackgroundGradient"]);
+        Display2Rows = SettingsHelper.ParseBool(element["Display2Rows"]);
+        ShowTotalHours = SettingsHelper.ParseBool(element["ShowTotalHours"], false);
+    }
+
+    public XmlNode GetSettings(XmlDocument document)
+    {
+        XmlElement parent = document.CreateElement("Settings");
+        CreateSettingsNode(document, parent);
+        return parent;
+    }
+
+    public int GetSettingsHashCode()
+    {
+        return CreateSettingsNode(null, null);
+    }
+
+    private int CreateSettingsNode(XmlDocument document, XmlElement parent)
+    {
+        return SettingsHelper.CreateSetting(document, parent, "Version", "1.6") ^
+        SettingsHelper.CreateSetting(document, parent, "TextColor", TextColor) ^
+        SettingsHelper.CreateSetting(document, parent, "OverrideTextColor", OverrideTextColor) ^
+        SettingsHelper.CreateSetting(document, parent, "TimeColor", TimeColor) ^
+        SettingsHelper.CreateSetting(document, parent, "OverrideTimeColor", OverrideTimeColor) ^
+        SettingsHelper.CreateSetting(document, parent, "BackgroundColor", BackgroundColor) ^
+        SettingsHelper.CreateSetting(document, parent, "BackgroundColor2", BackgroundColor2) ^
+        SettingsHelper.CreateSetting(document, parent, "BackgroundGradient", BackgroundGradient) ^
+        SettingsHelper.CreateSetting(document, parent, "Display2Rows", Display2Rows) ^
+        SettingsHelper.CreateSetting(document, parent, "ShowTotalHours", ShowTotalHours);
+    }
+
+    private void ColorButtonClick(object sender, EventArgs e)
+    {
+        SettingsHelper.ColorButtonClick((Button)sender, this);
+    }
+}
