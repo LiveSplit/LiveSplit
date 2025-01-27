a33871a8b9158bb2225570adae2ef0f7759637e6
Alex Knauth <alexander@knauth.org>
2025-01-27 15:11:30 -0500

    CommandServer: add getcustomvariablevalue (#2565)

--- src/LiveSplit.Core/Server/CommandServer.cs
+++ src/LiveSplit.Core/Server/CommandServer.cs
@@ -5,6 +5,7 @@ using System.IO.Pipes;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 using LiveSplit.Model;
@@ -491,6 +492,13 @@ public class CommandServer
                     Log.Warning($"[Sever] Split index {index} out of bounds for command {command}");
                 }
 
+                break;
+            }
+            case "getcustomvariablevalue":
+            {
+                string value = State.Run.Metadata.CustomVariableValue(args[1]);
+                // make sure response isn't null or empty, and doesn't contain line endings
+                response = string.IsNullOrEmpty(value) ? "-" : Regex.Replace(value, @"\r\n?|\n", " ");
                 break;
             }
             case "ping":
