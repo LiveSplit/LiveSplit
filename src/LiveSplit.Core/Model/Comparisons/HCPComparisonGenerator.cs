a2711f614b3f2643c94fd4ff22083f0815ac2f4b
Rodrigo Siqueira <rodrigo.siqueira.dev@gmail.com>
2025-03-17 23:18:45 +0100

    Adds HCPComparisonGenerator
    HCP is a way of calculating results in Golf. It's based of the last 20
    rounds played and averaging the top 8 results in that set.
    
    This is an application of this method to give runners a goal different
    from beating their PBs, which tends to be the case for a lot of runners.
    This new HCP goal aims for the runner to be able to improve their
    overall timings, or handicap as it's commonly referred to, without the
    common mental pressure of beating their best time ever, which can
    demotivate a lot of folks when they don't see their skills evolving.

---
+++ src/LiveSplit.Core/Model/Comparisons/HCPComparisonGenerator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using LiveSplit.Options;
+
+using static System.Math;
+
+namespace LiveSplit.Model.Comparisons;
+
+public class HCPComparisonGenerator : IComparisonGenerator
+{
+    public IRun Run { get; set; }
+    public const string ComparisonName = "Golf HCP";
+    public const string ShortComparisonName = "HCP";
+    public string Name => ComparisonName;
+    private const int NumRuns = 8;
+    private const int AverageSamplesCount = 20;
+
+    public HCPComparisonGenerator(IRun run)
+    {
+        Run = run;
+    }
+
+    protected TimeSpan CalculateAverage(IEnumerable<TimeSpan> curList)
+    {
+        double averageTime = curList.OrderBy(x => x.TotalSeconds).Take(AverageSamplesCount).ToList().Aggregate(0.0, (s, x) => s + x.TotalSeconds) / Min(AverageSamplesCount, curList.Count());
+        return TimeSpan.FromTicks((long)(averageTime * TimeSpan.TicksPerSecond));
+    }
+
+    public void Generate(TimingMethod method)
+    {
+        var allHistory = new List<List<TimeSpan>>();
+        foreach (ISegment segment in Run)
+        {
+            allHistory.Add([]);
+        }
+
+        // Get the last 20 complete runs 
+        var recentRuns = Run.AttemptHistory
+            .Where(attempt => Run.Last().SegmentHistory.TryGetValue(attempt.Index, out Time lastSegmentTime) && lastSegmentTime[method] != null)
+            .OrderByDescending(attempt => attempt.Index)
+            .Take(NumRuns);
+
+        foreach (Attempt attempt in recentRuns)
+        {
+            int ind = attempt.Index;
+            bool ignoreNextHistory = false;
+            foreach (ISegment segment in Run)
+            {
+                if (segment.SegmentHistory.TryGetValue(ind, out Time history))
+                {
+                    if (history[method] == null)
+                    {
+                        ignoreNextHistory = true;
+                    }
+                    else if (!ignoreNextHistory)
+                    {
+                        allHistory[Run.IndexOf(segment)].Add(history[method].Value);
+                    }
+                    else
+                    {
+                        ignoreNextHistory = false;
+                    }
+                }
+                else
+                {
+                    ignoreNextHistory = false;
+                }
+            }
+        }
+
+        TimeSpan? totalTime = TimeSpan.Zero;
+        for (int ind = 0; ind < Run.Count; ind++)
+        {
+            List<TimeSpan> curList = allHistory[ind];
+            if (curList.Count == 0)
+            {
+                totalTime = null;
+            }
+
+            if (totalTime != null)
+            {
+                totalTime += CalculateAverage(curList);
+            }
+
+            var time = new Time(Run[ind].Comparisons[Name]);
+            time[method] = totalTime;
+            Run[ind].Comparisons[Name] = time;
+        }
+    }
+
+    public void Generate(ISettings settings)
+    {
+        Generate(TimingMethod.RealTime);
+        Generate(TimingMethod.GameTime);
+    }
+}
