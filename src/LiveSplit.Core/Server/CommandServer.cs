6c306f72bb2e9a5a608cbb5b0e9585cb0ddc01b6
Alex Knauth <alexander@knauth.org>
2025-05-21 18:31:31 -0400

    Include System.Text.Json

--- src/LiveSplit.Core/Server/CommandServer.cs
+++ src/LiveSplit.Core/Server/CommandServer.cs
@@ -5,6 +5,7 @@ using System.IO.Pipes;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
