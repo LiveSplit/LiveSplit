da4f120dd863d9afda9cbc172e5edb7d1b4d0101
Christopher Serr <christopher.serr@gmail.com>
2026-08-08 19:37:23 +0200

    Update LiveSplit Core API Build
    Update livesplit-core to the merged configurable C API implementation together with the subsplit scrolling fix. Rebuild the parsing and image-shrinking native libraries from that revision.
    
    Write generated subset bindings under livesplit-core's target directory before copying the C# binding into the project. This keeps the intermediate bindings outside the SDK-style compile glob.

--- src/LiveSplit.Core/build_livesplit_core.sh
+++ src/LiveSplit.Core/build_livesplit_core.sh
@@ -1,6 +1,6 @@
 cd ../../lib/livesplit-core
 FEATURES=parsing,image-shrinking
-BINDINGS_DIR="$(pwd)/../../src/LiveSplit.Core/x64/target/bindings"
+BINDINGS_DIR="$(pwd)/target/livesplit-bindings"
 RUSTFLAGS="-C target-feature=+crt-static" cargo +stable-x86_64-pc-windows-msvc rustc --profile max-opt -p livesplit-core-capi --crate-type cdylib --no-default-features --features "$FEATURES" --target-dir ../../src/LiveSplit.Core/x64/target
 cp ../../src/LiveSplit.Core/x64/target/max-opt/livesplit_core.dll ../../src/LiveSplit.Core/x64/.
 RUSTFLAGS="-C target-feature=+crt-static" cargo +stable-i686-pc-windows-msvc rustc --profile max-opt -p livesplit-core-capi --crate-type cdylib --no-default-features --features "$FEATURES" --target-dir ../../src/LiveSplit.Core/x86/target
